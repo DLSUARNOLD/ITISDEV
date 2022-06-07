b66bbab540fba72adbf55bfbc1565bc567d7bfda
DLSUARNOLD <84303685+DLSUARNOLD@users.noreply.github.com>
2022-06-08 00:09:28 +0800

    initial files

---
+++ config/constants.php
@@ -0,0 +1,12 @@
+<?php
+
+define('HOST', 'localhost');
+define('USER', 'unaux_31905143');
+define('PASSWORD', 'passwords');
+define('DATABASE_NAME', 'ecommerceapp');
+
+define('CURRENCY', 'Rs');
+
+
+
+?>
