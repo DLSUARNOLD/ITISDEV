2b6a95302080f04bb65d3ada25a1780ca1ac12c3
DLSUARNOLD <84303685+DLSUARNOLD@users.noreply.github.com>
2022-06-08 00:13:30 +0800

    new constants.php

--- config/constants.php
+++ config/constants.php
@@ -1,9 +1,9 @@
 <?php
 
-define('HOST', 'localhost');
-define('USER', 'unaux_31905143');
-define('PASSWORD', 'passwords');
-define('DATABASE_NAME', 'ecommerceapp');
+define('HOST', 'us-cdbr-east-05.cleardb.net');
+define('USER', 'b7bc0f4776c024');
+define('PASSWORD', '1eb8cff2');
+define('DATABASE_NAME', 'heroku_16d6996744ccffd');
 
 define('CURRENCY', 'Rs');
 
